a99a59830106c966e3ae14ef8bf085481c8e2867
Gulsaba Fiha <sabafiha111@gmail.com>
2022-03-22 16:10:37 +0600

    deploying

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['skabelse.herokuapp.com']
+ALLOWED_HOSTS = ['https://skabelse.herokuapp.com/']
 
 
 # Application definition
